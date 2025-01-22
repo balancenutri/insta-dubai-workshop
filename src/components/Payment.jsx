e267c6f54943cbb8c95728b5225641a5f9eeb3fe
KundanChaudharyBN <Kundan.Chaudhary@balancenutrition.in>
2025-01-22 12:05:43 +0530

    create button disabled when loading

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -247,7 +247,7 @@ const RazorpayPayment = ({ setModal }) => {
           </button>
           <button
             type="submit"
-            className="w-full py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition duration-300 ease-in-out"
+            className="w-full py-2 bg-blue-500 disabled:bg-blue-400 text-white font-semibold rounded-lg hover:bg-blue-600 transition duration-300 ease-in-out"
             disabled={loading}
           >
             Register Now
